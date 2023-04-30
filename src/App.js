b207a51e1b1899b156ddb2da3d5c1dddb83e5780
sweeneyb <tab126@gmail.com>
2023-04-30 10:24:16 -0400

    github social works; UI is bad

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { createClient } from '@supabase/supabase-js'
+import { Auth } from '@supabase/auth-ui-react'
+import {
+  // Import predefined theme
+  ThemeSupa,
+} from '@supabase/auth-ui-shared'
+
+const supabase = createClient('https://yesvegksruagdpisofzv.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inllc3ZlZ2tzcnVhZ2RwaXNvZnp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODI4NjMwNzMsImV4cCI6MTk5ODQzOTA3M30.qyhrPb2tQXhQIOEnLdfVsn5dY5haDPA0xFvvVCYVljk')
 
 function App() {
   return (
     <div className="App">
+      <Auth supabaseClient={supabase}
+      appearance={{ theme: ThemeSupa }}
+      providers={['github']}/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
