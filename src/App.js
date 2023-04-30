40d83d3392c6e4f6946a12801af397b3c5321d6d
sweeneyb <tab126@gmail.com>
2023-04-30 10:30:46 -0400

    github auth works better; allows logout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
 import { createClient } from '@supabase/supabase-js'
 import { Auth } from '@supabase/auth-ui-react'
 import {
@@ -9,28 +10,38 @@ import {
 
 const supabase = createClient('https://yesvegksruagdpisofzv.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inllc3ZlZ2tzcnVhZ2RwaXNvZnp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODI4NjMwNzMsImV4cCI6MTk5ODQzOTA3M30.qyhrPb2tQXhQIOEnLdfVsn5dY5haDPA0xFvvVCYVljk')
 
+async function signOut() {
+  await supabase.auth.signOut();
+}
+
 function App() {
-  return (
-    <div className="App">
-      <Auth supabaseClient={supabase}
-      appearance={{ theme: ThemeSupa }}
-      providers={['github']}/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [session, setSession] = useState(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+
+    return () => subscription.unsubscribe()
+  }, [])
+
+  if (!session) {
+    return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} providers={['github']}/>)
+  }
+  else {
+    return (
+      <div>
+        <div>Logged in!</div>
+        <button onClick={signOut}>Sign Out</button>
+      </div>
+    )
+  }
 }
 
 export default App;
